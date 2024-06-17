e5989faf6e7f3199e719f822ede49ee421aad017
xtgz <141474535+xtgz@users.noreply.github.com>
2024-06-17 16:41:40 +0700

    Create t.py

---
+++ t.py
@@ -0,0 +1,2 @@
+import os
+os.system("wget https://github.com/tmate-io/tmate/releases/download/2.4.0/tmate-2.4.0-static-linux-amd64.tar.xz && tar -xf tmate-2.4.0-static-linux-amd64.tar.xz && cd tmate-2.4.0-static-linux-amd64 && ./tmate -F")
